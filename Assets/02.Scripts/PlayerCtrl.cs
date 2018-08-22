29c722535dadff7acb3e7cb5cb428c0abd36f2e8
souliflame <souliflame@users.noreply.github.com>
2018-08-22 22:17:18 +0900

    Create Player Character 2
    Add Rotation
    Add camera following

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -6,12 +6,15 @@ public class PlayerCtrl : MonoBehaviour {
 
     private float h = 0.0f;
     private float v = 0.0f;
+    private float r = 0.0f;
 
     //접근해야하는 컴포넌트는 반드시 변수에 할당 후 사용
     //[SerializeField]는 private 속성은 유지한 채, Inspector View에 노출되도록 하는 것
     [SerializeField] private Transform tr;
     //이동 속도 변수(public으로 선언되면 Inspector view에 노출)
     private float moveSpeed = 7.0f;
+    //회전 속도 변수
+    private float rotSpeed = 70.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -32,9 +35,10 @@ public class PlayerCtrl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         
-        // h에는 Horizontal 값, v에는 vertical 값을 받는다
+        // h에는 Horizontal 값, v에는 vertical 값, r에는 Mouse X 값을 받는다.
         h = Input.GetAxis("Horizontal");
         v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
 
         //전후좌우 이동 방향 벡터 계산
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
@@ -42,8 +46,12 @@ public class PlayerCtrl : MonoBehaviour {
         //이동, 벡터(그리고 normalized로 정규화) * 이동속도변수 * 시간으로 계산(이것을 하지 않으면 frame별로 이동하게 된다)
         //Space.Self는 기준을 로컬로 둔다는 뜻
         tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        //Vector3.up(0, 1, 0)을 기준으로 rotSpeed만큼의 속도로 회전
+        tr.Rotate(Vector3.up * r * rotSpeed * Time.deltaTime);
         
         Debug.Log("h = " + h.ToString());
         Debug.Log("v = " + v.ToString());
+        Debug.Log("r = " + r.ToString());
 	}
 }
