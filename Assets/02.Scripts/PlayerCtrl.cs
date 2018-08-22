824d38fe26ee5830fa43e7047e653619a915d1c7
souliflame <souliflame@users.noreply.github.com>
2018-08-22 22:51:19 +0900

    Create Player Character 3
    Add Animation, animation scripts

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -2,6 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//클래스는 System.Serializable이란 Attribute를 명시해야 Inspector View에 노출
+[System.Serializable]
+public class PlayerAnim
+{
+    public AnimationClip idle;
+    public AnimationClip runF;
+    public AnimationClip runB;
+    public AnimationClip runR;
+    public AnimationClip runL;
+}
+
 public class PlayerCtrl : MonoBehaviour {
 
     private float h = 0.0f;
@@ -11,10 +22,16 @@ public class PlayerCtrl : MonoBehaviour {
     //접근해야하는 컴포넌트는 반드시 변수에 할당 후 사용
     //[SerializeField]는 private 속성은 유지한 채, Inspector View에 노출되도록 하는 것
     [SerializeField] private Transform tr;
+    
     //이동 속도 변수(public으로 선언되면 Inspector view에 노출)
-    private float moveSpeed = 7.0f;
+    private float moveSpeed = 5.0f;
     //회전 속도 변수
     private float rotSpeed = 70.0f;
+
+    //Inspector View에 표시할 애미네이션 클래스 변수
+    public PlayerAnim playerAnim;
+    //애니메이션 컴포넌트를 저장하기 위한 변수
+    public Animation anim;
 
 	// Use this for initialization
 	void Start () {
@@ -29,6 +46,12 @@ public class PlayerCtrl : MonoBehaviour {
         tr = GetComponent("Transform") as Transform;
         tr = (Transform)GetComponent(typeof(Transform));
         */
+
+        //Animation 컴포넌트를 변수에 할당
+        anim = GetComponent<Animation>();
+        //Animation 컴포넌트의 애니메이션 클립을 지정하고 실행
+        anim.clip = playerAnim.idle;
+        anim.Play();
 
 	}
 	
@@ -49,6 +72,29 @@ public class PlayerCtrl : MonoBehaviour {
 
         //Vector3.up(0, 1, 0)을 기준으로 rotSpeed만큼의 속도로 회전
         tr.Rotate(Vector3.up * r * rotSpeed * Time.deltaTime);
+
+        //키보드 입력값을 기준으로 동작할 애니메이션 수행
+        if (v > 0.1f)
+        {
+            anim.CrossFade(playerAnim.runF.name, 0.3f); //전진 애니메이션
+        }
+        else if (v < -0.1f)
+        {
+            anim.CrossFade(playerAnim.runB.name, 0.3f); //후진 애니메이션
+        }
+        else if (h > 0.1f)
+        {
+            anim.CrossFade(playerAnim.runR.name, 0.3f); //오른쪽 이동 애니메이션
+        }
+        else if( h < -0.1f)
+        {
+            anim.CrossFade(playerAnim.runL.name, 0.3f); //왼쪽 이동 애니메이션
+        }
+        else
+        {
+            anim.CrossFade(playerAnim.idle.name, 0.3f); //정지 시 Idle 애니메이션
+        }
+
         
         Debug.Log("h = " + h.ToString());
         Debug.Log("v = " + v.ToString());
