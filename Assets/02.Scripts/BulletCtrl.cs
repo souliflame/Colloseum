ce4d0dc523f16bcd53d690a8f6b023b98431c36e
souliflame <souliflame@users.noreply.github.com>
2018-08-27 23:20:07 +0900

    Create Bullet 1
    Create Bullet and add initial script.

---
+++ Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour {
+
+    //총알의 파괴력
+    public float damage = 20.0f;
+
+    //총알의 발사 속도
+    public float speed = 2000.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<Rigidbody>().AddRelativeForce(transform.forward * speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
