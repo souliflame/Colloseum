05c2d86a7c0e5cdf433f7929cf97f4c0da3c2a7d
souliflame <souliflame@users.noreply.github.com>
2018-09-07 17:38:56 +0900

    Create Bullet 4
    [Add] CartridgeEject, Muzzle Flash Effect

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -8,10 +8,15 @@ public class FireCtrl : MonoBehaviour {
     public GameObject bullet;
     //총알 발사 좌표
     public Transform firePos;
-
+    //탄피 추출 파티클
+    public ParticleSystem cartridge;
+    //총구 화염 파티클
+    private ParticleSystem muzzleFlash;
+    
 	// Use this for initialization
 	void Start () {
-		
+        //FirePos 하위에 있는 컴포넌트 추출
+        muzzleFlash = firePos.GetComponentInChildren<ParticleSystem>();
 	}
 	
 	// Update is called once per frame
@@ -21,12 +26,15 @@ public class FireCtrl : MonoBehaviour {
         {
             Fire();
         }
-		
 	}
 
     void Fire()
     {
         //Bullet 프리팹을 동적으로 생성
         Instantiate(bullet, firePos.position, firePos.rotation);
+        //파티클 실행
+        cartridge.Play();
+        //머즐 파티클 실행
+        muzzleFlash.Play();
     }
 }
